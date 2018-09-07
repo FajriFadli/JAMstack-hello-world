77f180eedcddc31aa377198f14f8ff453aeb3aba
Fajri Fadli <fajri.fadli104@gmail.com>
2018-09-07 13:21:38 +0700

    Initialize repo

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hello World</h1>
+    <p>I'm Fajri Fadli.</p>
+    <p>Let's get started with the JAMstack Hackathon!</p>
+  </div>
+)
+
+export default IndexPage
